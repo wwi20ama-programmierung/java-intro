46bd8023a0249e94bb773f1815e4822998d24309
Reiner Hüchting <reiner.huechting@gmx.de>
2021-03-19 16:56:02 +0100

    Vorlesungsmaterial vom 15.3. und 19.3. hinzugefügt.

---
+++ Vorlesungsinhalte/Vorlesung 19.03.2021/Repl_Baeume/Baum.java
@@ -0,0 +1,23 @@
+public class Baum {
+  private Knoten wurzel;
+
+  public Baum() {
+    wurzel = new Knoten();
+  }
+
+  // Liefert true, wenn der Baum leer ist.
+  public boolean isEmpty() {
+    return wurzel.isDummy();
+  }
+
+  // Sucht ein Element im Baum und liefert den engl. String.
+  public String get(String de) {
+    // TODO
+    return "";
+  }
+
+  // Fuegt ein Element zum Baum hinzu.
+  public void put(String de, String en) {
+    // TODO
+  }
+}
---
+++ Vorlesungsinhalte/Vorlesung 19.03.2021/Repl_Baeume/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Baum b1 = new Baum();
+
+    System.out.println(b1.isEmpty());
+  }
+}
